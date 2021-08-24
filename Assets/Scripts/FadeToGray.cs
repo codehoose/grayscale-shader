4dd2fe4bfd147f4517550f981d5f17aece7e85ea
Sloan Kelly <sloan@codehoose.com>
2021-08-23 21:16:33 -0400

    Video 2 - Fix for material value perma-set

--- Assets/Scripts/FadeToGray.cs
+++ Assets/Scripts/FadeToGray.cs
@@ -1,28 +1,26 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FadeToGray : MonoBehaviour
 {
-    public Material[] materials;
+    public Renderer[] renderers;
 
 
     public void DoTheFade(int index)
     {
-        StartCoroutine(DoTheFadeCR(materials[index]));
+        StartCoroutine(DoTheFadeCR(renderers[index]));
     }
 
-    private IEnumerator DoTheFadeCR(Material material)
+    private IEnumerator DoTheFadeCR(Renderer material)
     {
         float time = 0;
         while (time < 1f)
         {
-            material.SetFloat("_Blend", time);
+            material.material.SetFloat("_Blend", time);
             time += Time.deltaTime;
             yield return null;
         }
 
-        material.SetFloat("_Blend", 1);
+        material.material.SetFloat("_Blend", 1);
     }
 }
